72e36932f091cf645ab3a638fd7928454ca8466e
Gabriel Motta <gabrielmottadev@gmail.com>
2021-12-13 04:18:17 -0300

    Home component changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('header') header! : ElementRef;
+  
+  isSticky = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    window.onscroll = () => (this.testFunction());
+  }
+  
+  testFunction(): void {
+    let sticky = this.header.nativeElement.offsetTop;
+    if (window.pageYOffset > sticky) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
   }
 
 }
