7cd8e2c6c5f3163ad02c224ac704099642d87b0d
Gabriel Motta <gabrielmottadev@gmail.com>
2021-12-14 10:13:35 -0300

    Added button to change locale

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -48,6 +48,38 @@ export class HomeComponent implements OnInit, AfterViewInit {
     // });
   }
 
+  changeLocale(): void {
+    let newLocale = 'pt-br';
+    switch (this.localeService.currentLocale.locale) {
+      case 'en':
+        newLocale = 'pt-br'
+        break;
+      case 'pt-br':
+        newLocale = 'es';
+        break;
+      case 'es':
+        newLocale = 'en';
+        break;
+      default:
+        newLocale = "pt-br";
+        break;
+    }
+    this.localeService.setLocale(newLocale);
+  }
+
+  getLocaleImage() {
+    switch (this.localeService.currentLocale.locale) {
+      case 'en':
+        return "url('/assets/paises/bandeira_inglaterra.jpg')";
+      case 'pt-br':
+        return "url('/assets/paises/bandeira_brasil.jpg')";
+      case 'es':
+        return "url('/assets/paises/bandeira_espanha.jpg')";
+      default:
+        return "url('/assets/paises/bandeira_brasil.jpg')";
+    }
+  }
+
   openWhatsapp(numero: string): void {
     let url = 'https://wa.me/' + numero + '/?text=Ol%C3%A1%2C%20gostaria%20de%20contratar%20os%20servi%C3%A7os%20da%20CenavRio.';
     window.open(url, '_blank')!.focus();
