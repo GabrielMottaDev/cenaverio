d703edbb15310cb7046fde3fc750e73c5c2109eb
Gabriel Motta <gabrielmottadev@gmail.com>
2021-12-14 03:11:02 -0300

    Clientes grid improved for customization

--- src/app/shared/cliente.ts
+++ src/app/shared/cliente.ts
@@ -1,4 +1,5 @@
 export interface Cliente {
     name?:string;
     image:string;
+    customStyle?:object;
 }
--- src/app/views/clientes-grid/clientes-grid.component.ts
+++ src/app/views/clientes-grid/clientes-grid.component.ts
@@ -18,7 +18,10 @@ export class ClientesGridComponent implements OnInit {
       image: 'logo_biocom.png'
     },
     {
-      image: 'logo_br.png'
+      image: 'logo_br.png',
+      // customStyle: {
+      //   'mix-blend-mode': 'multiply'
+      // }
     },
     {
       image: 'logo_cspm.png'
@@ -36,7 +39,10 @@ export class ClientesGridComponent implements OnInit {
       image: 'logo_inter_express.png'
     },
     {
-      image: 'logo_iss_shipping.png'
+      image: 'logo_iss_shipping.png',
+      customStyle: {
+        'filter': 'drop-shadow(#00000091 0px 0px 2px)'
+      }
     },
     {
       image: 'logo_jumbo.png'
@@ -90,7 +96,10 @@ export class ClientesGridComponent implements OnInit {
       image: 'logo_unimar.png'
     },
     {
-      image: 'logo_wilhelmsen.png'
+      image: 'logo_wilhelmsen.svg',
+      customStyle: {
+        'filter': 'drop-shadow(1px 1px 2px white)'
+      }
     },
     {
       image: 'logo_wilson_sons.png'
@@ -100,6 +109,44 @@ export class ClientesGridComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  applyFilter(cliente: Cliente): object {
+    if(cliente.customStyle){
+      return cliente.customStyle;
+    } else {
+      return {};
+    }
+  }
+
+  public async removeImageBackground(image: any) {
+    const backgroundColor = { red: 255, green: 255, blue: 255 };
+    const threshold = 10;
+  
+    const imageElement = new Image();
+    imageElement.src = image;
+    await new Promise(function(resolve) { imageElement.addEventListener('load', resolve); });
+  
+    var canvas = document.createElement('canvas');
+    canvas.width = imageElement.naturalWidth;
+    canvas.height = imageElement.naturalHeight;
+  
+    var ctx = canvas.getContext('2d');
+    ctx!.drawImage(imageElement, 0, 0);
+    const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < imageData.data.length; i += 4) {
+      const red = imageData.data[i];
+      const green = imageData.data[i + 1];
+      const blue = imageData.data[i + 2];
+      if (Math.abs(red - backgroundColor.red) < threshold &&
+        Math.abs(green - backgroundColor.green) < threshold &&
+        Math.abs(blue - backgroundColor.blue) < threshold) {
+        imageData.data[i + 3] = 0;
+      }
+    }
+  
+    ctx!.putImageData(imageData, 0, 0);
+    return canvas.toDataURL(`image/png`);
   }
 
 }
